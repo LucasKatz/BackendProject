e71d60ee79c3f7a22a4d0269e5530aa8fe15959a
Lucas Katz <l.katz92@gmail.com>
2022-12-20 16:21:22 -0300

    intentando resolver desafio2

--- Desafio2.js
+++ Desafio2.js
@@ -10,160 +10,160 @@ class ProductManager {
       this.products = this.readFile()
 
     }
-
-    readFile () {
-      const data = JSON.parse(fs.readFileSync('./$(this.path)', 'utf-8'))
-      return data 
+    writeData(){
+        let dataString = JSON.stringify(data);
+        fs.writeFileSync(`./${this.path}`, dataString)
     }
 
-    addProducts() {
+    readFile () {
+        const data = JSON.parse(fs.readFileSync('./$(this.path)', 'utf-8'))
+        return data 
+    }
+
+    addProducts(product) {     
         
-      
-        const product= ({
-            title: this.title,
-            description: this.description,
-            price: this.price,
-            thumbnail: this.thumbnail,
-            code: this.code,
-            stock: this.stock,
-            id:ProductManager.id
-          
-        })
+        //Aca llamar this o crear variable trayendo los products usando readfile() o getProducts()
+        let listado = this.readFile();
+        const checkInCart = this.listado.find(p => p.id === product.code)
 
-        getProducts () {
-          try {
-            const productos =  fs.promises.readFile(this.file, 'utf-8')
-             return JSON.parse(productos)    
-             
-         } catch (err) {
-             if(err.message.includes ('no such file or directory')) return [];
-             console.log("Oops! There has been a mistake")
-         }
-         console.log(products)
-        }    
-    
-    writeFile = async () =>  {
-      try {
-          await fs.promises.writeFileSync((ProductManager,products.JSON),
-            this.path, JSON.stringify(products))
-          } catch (err) {
-          console.log("Oops! There has been a mistake")
-          }
-        }
-
-    
-
-        const checkInCart = products.find(element => element.code === product.code)
-        if(checkInCart == true) {
-            console.log("ERROR - Please check the information and try again")
-        }
-        else {
-            products.push(product)
-            ProductManager.id++
+        if (!product.title || !product.description || !product.price ||
             
-        } 
-
-       
-          if (!product.title || !product.description || !product.price ||
-
             !product.thumbnail || !product.code || !product.stock) {
-  
-          throw new Error('Todos los campos son obligatorios');
-  
+                
+                throw new Error('Todos los campos son obligatorios'); 
+            } else if (checkInCart){
+                console.log("ERROR - Please check the information and try again")
+            }
+        else {
+            
+            
+            product.id = products.length > 0 ? products[products.length - 1].id + 1 : 1;
+            products.push(product)
+            this.writeFile(productos.JSON)
+                             
         }
-          
-        }
-      
-    }  
+    }
+            
+         
+        
+    getProducts () {
+        try {
+          const productos =  fs.promises.readFile(this.path, 'utf-8')
+           return JSON.parse(productos)    
+           
+       } catch (err) {
+           if(err.message.includes ('no such file or directory')) return [];
+           console.log("Oops! There has been a mistake")
+       }
+       console.log(products)
+      }    
+    
+    writeFile(products){  
+        try {
+             fs.promises.writeFileSync(this.path,JSON.stringify(products))
+            } catch (err) {
+            console.log("Oops! There has been a mistake")
+            }      
+    }    
+    
 
 
-
-const isInCart = (id) => { return products.find (product =>product.title ===title) }
-
+// const isInCart = (id) => { return products.find (product =>product.title ===title) }
 
 
+getProductsById (id){
+    //Aca crear una variable trayendo los productos usando getProducts() o readFile() 
+    let listaProductos = this.readFile();
+    const products = this.listaProductos
+    // const products = this.xxxx
 
-getProductsById (id)  {
-
-  const search = getProducts.find(product => product.id === id) //asi el find??
+  const search = products.find(product => product.id === id) //asi el find??
 
 if (search == undefined) {
   console.log( "Product not found")
 }
 else {
-  console.log(search)
+    //retornarlo para cuando se llame el metodo
+  return search 
 }
 }
-const isInProducts = (title) => {
+
+isInProducts  (title)  {
     products.find (prod => prod.title === title)
 }
 
 
 
 
-updateProduct = () => {const arr = [ProductManager];
+updateProduct(id, product){
   
-  const newArr = arr.map(object => {
-    if (object.id === 2) {
-    
-      return {...object, //valor a sobreescribir
-    };
+    let data = this.readFile ();
+    if(data.find(product=>product.id===id)){
+        let productDeleted = data.filter(product => product.id!==id)
+        product.id=id;
+        productDeleted.push(product);
+        this.writeData(productDeleted);
+        return productDeleted;
+
     }
-    return object;
-  });
-  
-  console.log(newArr);   
+    else{
+        console.log('The product to be updated does not exist')
+    }
 }
 
 
-deleteProduct  = async id =>  {
-    let productos = await  this.getAll()
+async deleteProduct (id){
+    let productos = await  this.readFile(productos.JSON) //Aca no estes getAll debes llamar getProducts() o readFile()
     try {
        productos = productos.filter (producto =>producto.id != id )
-    await this.writeFile(productos)
+    this.writeFile(productos)
         
     } catch (err) {
         console.log("Oops! There has been a mistake")
     }
 }
 
-deleteAll = async => {
+deleteAll(){
     this.writeFile([])
 }
-
-const prod1 = new ProductManager(productos.JSON)
-
-const prod2 = new ProductManager(productos.JSON)
-
-//const prod3 = new ProductManager("", 3)
+}
 
 
-ProductManager.addProducts({
+const prod1 = new ProductManager('./this.path')//aca pasar ruta del archivo a crear lo toma this.path
+
+
+
+
+prod1.updateProduct(1,{
             title: "Lámpara Tokio",
             description: "Lámpara escritorio aluminio negro led",
             price: 2200,
             thumbnail: "ruta de imagen",
             code: 101,
             stock: 3,
-            id:ProductManager.id,
+}) //Los productos pasarlo por separado y podes usar prod1 ya que estas instanciando a ProducManager
 
-            title: "Lampara Double Sh",
-            description: "Embutido retraible doble cabezal móvil aluminio blanco led",
-            price: 3200,
-            thumbnail: "ruta de imagen",
-            code: 102,
-            stock: 4,
-            id:ProductManager.id
-})
+prod1.updateProduct(2,{
+    title: "Lámpara Tokio",
+    description: "Lámpara escritorio aluminio negro led",
+    price: 3200,
+    thumbnail: "ruta de imagen",
+    code: 102,
+    stock: 4,
+}) 
+
+prod1.updateProduct(3,{
+    title: "Lámpara Tokio",
+    description: "Lámpara escritorio aluminio negro led",
+    price: 2500,
+    thumbnail: "ruta de imagen",
+    code: 103,
+    stock: 5,
+}) 
 
 
-getProducts()
 
-
-
-
-
-
-
-
-
+//Aca ejemplo como se debrian llamar 
+//   console.log(prod1.getProductById(1));  
+//   prod1.deleteProduct(6);
+//   console.log(prod1.getProducts());
