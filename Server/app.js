9dabc039c0bc1f7c9beffdecf04415ed384f7a3f
Lucas Katz <l.katz92@gmail.com>
2022-12-26 14:12:36 -0300

    Avanzando desafio 3

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,21 @@
 const express = require ('express')
 const app = express()
+const ProductManager = require ('../desafio3')
+const productManager = new ProductManager('../database/productos.JSON')
 
-app.get ('/', (req,res)=>{
-    
+app.get ('/products', async (req,res)=>{
+const products = await productManager.getProducts();
+const {limit} = req.query
+if (limit) return res.json(products.slice(0,limit))
+else return res.json(products)
+})
+
+app.get ('/products/pid', async (req,res)=>{
+    const products = await productManager.getProducts();
+    const {pid} = req.params
+    const product = product.find (products => products.id === id)
+})
+
+app.listen(3000, () => {
+
 })
