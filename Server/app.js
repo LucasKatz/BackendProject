b9c13671c3dcc89f61039f72db88fae1ab0bf437
Lucas Katz <l.katz92@gmail.com>
2022-12-27 18:44:42 -0300

    desafio 3 finalizado

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,7 @@
 const express = require ('express')
 const app = express()
 const ProductManager = require ('../desafio3')
-const productManager = new ProductManager('../database/productos.JSON')
+const productManager = new ProductManager('./database/productos.JSON')
 const PORT = 8080
 
 
@@ -12,10 +12,10 @@ if (limit) return res.json(products.slice(0,limit))
 else return res.json(products)
 })
 
-app.get ('/products/pid', async (req,res)=>{
+app.get ('/products/:pid', async (req,res)=>{
     const products = await productManager.getProducts();
     const {pid} = req.params
-    const product = product.find (products => products.id === pid)
+    const product = products.find (products => products.id === parseInt(pid))
 
     if (product) return res.status(200).json(product)
     else return res.status(404).json({message:'Product not found'});
