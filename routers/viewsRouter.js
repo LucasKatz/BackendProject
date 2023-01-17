ecaf7fa75e4b8661cebc29c08b033535f240124d
Lucas Katz <l.katz92@gmail.com>
2023-01-17 15:25:14 -0300

    Termine websocket?

--- routers/viewsRouter.js
+++ routers/viewsRouter.js
@@ -3,6 +3,17 @@ const viewsRouter = express.Router();
 const fs = require ('fs')
 
 viewsRouter.get ("/", async (req, res) => { 
-    const products = await readFile(); 
+    const products = await readFile(); //agrego archivo como parametro entre ()? el JSON o el handlebars?
     res.render("home", {products} );
 });
+
+viewsRouter.get('/realtimeproducts', async (req, res)=>{
+    let products = fs.writeFileSync()//agrego archivo como parametro entre ()? el JSON o el handlebars? y aca write porque reescribe y actualiza el archivo??;
+
+    req.io.on('connection', socket=>{
+        console.log("Cliente conectado.");
+    })
+
+    res.render('realTimeProducts', {newProductToAdd});
+})
+
