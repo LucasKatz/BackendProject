9796494d07f8e2abf153b38e19e22c110f9f09c9
Lucas Katz <l.katz92@gmail.com>
2023-01-26 14:02:41 -0300

    correcciones websocket

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 //Estos imports que dejo aca, son necesarios para usarlos abajo con app.use... o estoy haciendo lo mismo 2 veces??
-
+import __dirname from './utils.js';
 import express from 'express';
 import handlebars from 'express-handlebars';
 import { Server } from 'socket.io';
 
-// Instancia de expres y servidor.
-const express = require ('express')
-const app = express()
+import viewsRouter from './routers/viewsRouter.js';
 
-//const ProductManager = require ('../desafio3')
-//const productManager = new ProductManager('./database/productos.JSON')
+
 const PORT = 8080
 
 const socketServer=new Server(httpServer);
-app.listen(PORT, () => {
-    console.log (`Server running on port ${PORT}`)
-    })
+
+socketServer.on('connection', (socket) => {
+    socket.on('mensaje',(msj) => {
+        console.log('Recibi un mensaje que dice: '+msj);
+    });
+})
+//a esto te referis con instanciar?
 
 const httpServer = app.listen(PORT, () => {
 
     console.log (`Server running on port ${PORT}`)
-        
+    
     })    
 
 
@@ -36,37 +37,6 @@ app.use(express.urlencoded({extended:true}));
 
 
 // Rutas.
-app.use('/api/carts', cartsRouter);
-app.use('/api/products', productsRouter);
 app.use('/', viewsRouter);
 
-//app.get('/', async(req,res) => {
-//    res.render('home',{});
-//});
-
-//socketServer.on('connection', (socket) => {
-//    socket.on('mensaje',(msj) => {
-//        console.log('Recibi un mensaje que dice: '+msj);
-//    });
-//    socket.emit('singlecast', 'Este es un mensaje singlecast');
-//});
-
-//Teniendo todo en viewsRouter esto no es necesario, no?
-
-
-//app.get ('/products', async (req,res)=>{
-//const products = await productManager.getProducts();
-//const {limit} = req.query
-//if (limit) return res.json(products.slice(0,limit))
-//else return res.json(products)
-//})
-
-//app.get ('/products/:pid', async (req,res)=>{
-    //const products = await productManager.getProducts();
-    //const {pid} = req.params
-    //const product = products.find (products => products.id === parseInt(pid))
-
-    //if (product) return res.status(200).json(product)
-    //else return res.status(404).json({message:'Product not found'});
-//})
 
