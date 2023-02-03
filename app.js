a994d703e677415894bbee8b214d8277a3ce3307
Lucas Katz <l.katz92@gmail.com>
2023-02-03 14:47:44 -0300

    armando chat

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import __dirname from './utils.js';
+import mongoose from 'mongoose';
+import chatRoute from './routers/chatRouter.js'
 import express from 'express';
 import handlebars from 'express-handlebars';
 import { Server } from 'socket.io';
@@ -8,6 +10,8 @@ import viewsRouter from './routers/viewsRouter.js';
 const app = express()
 const PORT = 8080
 
+const messages = []
+
 // Configuracion Express.
 app.engine('handlebars',handlebars.engine());
 app.set('views',__dirname+'/views');
@@ -16,6 +20,7 @@ app.set('view engine','handlebars');
 app.use(express.static(__dirname+'/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use("/chat", chatRoute);
 
 // Rutas.
 app.use('/', viewsRouter);
