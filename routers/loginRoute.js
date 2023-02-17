c7273b92cc1dee39d6b158ea5110f6daee309dbb
Lucas Katz <l.katz92@gmail.com>
2023-02-17 19:05:53 -0300

    changes

--- routers/loginRoute.js
+++ routers/loginRoute.js
@@ -7,4 +7,8 @@ router.get("/", async (req, res) => {
     res.render("login", { style: "css/login.css" });
 });
 
+router.post("/", async (req, res) => {
+    const {username, password}=req.body;
+});
+
 export default router;
