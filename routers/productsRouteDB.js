cf4d7f5f6e1b9be235a458a0ecc4a511d16013b2
Lucas Katz <l.katz92@gmail.com>
2023-02-18 22:33:31 -0300

    ruta get

--- routers/productsRouteDB.js
+++ routers/productsRouteDB.js
@@ -4,23 +4,66 @@ import { ProductManager } from "../DAO/Class/DataBaseManager.js";
 const router = Router();
 const productManager = new ProductManager();
 
-router.get("/", async(req, res) => {
+router.get("/", async (req, res) => {
+  const stock = req.query.stock;
+  const page = req.query.page;
+  const limit = req.query.limit || 10;
+  const sort = req.query.sort || 1;
+  let query;
+  let prevURL;
+  let nextURL;
+
+  const url = req.protocol + "://" + req.get("host") + req.originalUrl;
+  const category = req.query.category;
+  if (category != undefined || stock != undefined) {
+    if (category != undefined) {
+      query = { category: category };
+    } else {
+      query = { stock: stock };
+    }
+  } else {
+    if (category != undefined && stock != undefined) {
+      query = { category: category, stock: stock };
+    } else {
+      query = {};
+    }
+  }
   try {
-    const category = req.query.category;
-
-    const stock = req.query.stock;
-
-    const page = req.query.page || 1;
-
-    const limit = req.query.limit || 10;
-
-    const sort = req.query.sort || null;
-
-    const url = req.protocol + '://' + req.get('host') + req.originalUrl;
-    const product = await productManager.read();
-    res.send(product);
+    const respuesta = await productModel.paginate(
+      query,
+      {
+        page: page || 1,
+        limit: limit,
+        sort: { price: sort },
+      },
+      (err, res) => {
+        res.hasPrevPage
+          ? (prevURL = url.replace(`page=${res.page}`, `page=${res.prevPage}`))
+          : null;
+        res.hasNextPage
+          ? (nextURL =
+              page == undefined
+                ? url.concat(`&page=${res.nextPage}`)
+                : url.replace(`page=${res.page}`, `page=${res.nextPage}`))
+          : null;
+        return {
+          status: res.docs.length != 0 ? "success" : "error",
+          payload: res.docs,
+          totalPages: res.totalPages,
+          prevPage: res.prevPage,
+          nextPage: res.nextPage,
+          page: res.page,
+          hasPrevPage: res.hasPrevPage,
+          hasNextPage: res.hasNextPage,
+          prevLink: prevURL,
+          nextLink: nextURL,
+        };
+      }
+    );
+    res.send(respuesta);
   } catch (err) {
-    res.status(500).send(err.message);
+    console.log(err);
+    res.send(err);
   }
 });
 
