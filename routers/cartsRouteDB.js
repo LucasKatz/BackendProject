0cb6b2311b1049992d79ce0b02528be5c28e182c
Lucas Katz <l.katz92@gmail.com>
2023-03-08 20:43:28 -0300

    Correcciones

--- routers/cartsRouteDB.js
+++ routers/cartsRouteDB.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { CartManager } from "../DAO/Class/DataBaseManager.js";
 import productModel from "../DAO/models/productsModel.js";
+import cartModel from "../DAO/models/cartsModel.js";
 
 const router = Router();
 const cartManager = new CartManager();
@@ -9,8 +10,14 @@ const cartManager = new CartManager();
 //ruta que lee los productos que hay dentro de un carrito
 router.get("/:cid", async(req, res) => {
   try {
-    const cart = await cartManager.read();
-    res.send(cart);
+    const findCart = await cartModel.findOne({_id:cartId}).populate('products.product')
+    if(findCart !=undefined){
+    res.status(200).send(findCart)
+    return findCart
+    }
+    else{
+      res.status(400).send("El Cart solicitado no existe")
+    }
   } catch (err) {
     res.status(500).send(err.message);
   }
@@ -43,7 +50,7 @@ router.post('/:cid/product/:pid', async (req, res)=>{
 
 
 //ruta que borra el carrito completo
-router.delete("/api/carts/:cid", async (req, res) => {
+router.delete("/:cid", async (req, res) => {
   const { id } = req.params;
   try {
     const response = await cartManager.delete(id);
