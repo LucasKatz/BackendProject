ef8f593b6bf5c2816c6a1b892ee9fcfb0489601c
Lucas Katz <l.katz92@gmail.com>
2023-03-08 21:01:57 -0300

    correcciones a 2da pref

--- routers/cartsRouteDB.js
+++ routers/cartsRouteDB.js
@@ -34,7 +34,7 @@ router.post("/", async(req, res) => {
 });
 
 // Agrega un producto al carrito.
-router.post('/:cid/product/:pid', async (req, res)=>{
+/*router.post('/:cid/product/:pid', async (req, res)=>{
   try {
       let productId = req.params.pid;
       let cartId = req.params.cid;
@@ -45,8 +45,68 @@ router.post('/:cid/product/:pid', async (req, res)=>{
   } catch (error) {
       res.status(404).send({status:'error', message: error.message});
   }
-})
+})*/
 
+
+//RUTA POST QUE AGREGA PRODUCTO A CARRITO CON EL FORMATO PEDIDO EN DIAPOS
+
+// La ruta api/carts/:cid/products/:pid (método post) agrega un producto a un carrito
+router.post("/:cid/products/:pid", async (req, res) => {
+  const  cartId  = req.params.cid;
+  const productId = req.params.pid
+  const {quantity} = req.body;
+
+  //Comprobación de la estructura y validez de la Id de producto y la Id del carrito recibidos por parámetro
+  if(productId.trim().length!=24)
+  { 
+    res.status(400).send({error: "La Id de producto ingresada no es válida"})
+    return
+  }
+  else if(cartId.trim().length!=24){
+    res.status(404).send({error: "La Id del Cart ingresada no es válida"})
+    return
+  }
+  const productExist = await productModel.findById(productId)
+  const cartExist = await cartModel.findById(cartId)
+
+  if(productExist==null){
+    res.status(400).send({error:"No existe un producto con la Id ingresada"})
+    return
+  }
+  else if(cartExist==null){
+    res.status(404).send({error:"No existe un Cart con la Id ingresada"})
+    return
+  }
+
+  //Comprobación de stock
+  let checkStock = productExist.stock
+  if(parseInt(quantity)>checkStock){
+    res.status(400).send({status:"Error", message:"No hay stock suficiente"})
+  return
+  }
+
+  //Si se comprueba la validez de los parámetros se ejecutan las acciones para agregar el producto al carrito
+  try {
+    let selectedCart = await cartModel.find({_id: cartId})
+    let productExistInCart = selectedCart[0].products.find((product)=>product.product == productId)
+
+    if(productExistInCart == undefined){
+      selectedCart[0].products.push({product: productId, quantity: quantity})
+    }
+    else{
+      let newQuantity = productExistInCart.quantity + quantity
+      let productIndex = selectedCart[0].products.findIndex((product)=> product.product == productId)
+      selectedCart[0].products[productIndex].quantity = newQuantity
+    }
+
+    let result = await cartModel.updateOne({_id:cartId,},selectedCart[0])
+
+
+    res.status(200).send({ message: "Producto agregado al carrito", selectedCart });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 
 //ruta que borra el carrito completo
