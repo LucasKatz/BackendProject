42ba57c9ac3659df4829f053d956d5f0ca94ca08
Lucas Katz <l.katz92@gmail.com>
2023-04-18 14:38:41 -0300

    agregue navbar a todas las views

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ export const  authMiddleware = async (req,res,next)=>{
     if(req.session?.user != undefined){
     return next()
     }
-    return res.status(401).redirect("/login")
+    return res.status(401).redirect("/login?message=Debe loguearse para acceder a esta seccion")
     
 }
 export default authMiddleware
