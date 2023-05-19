8d7f0cdfdad9ffaf55d54ca5b3a9ab3fac97b5de
Lucas Katz <l.katz92@gmail.com>
2023-05-19 18:12:59 -0300

    funciona signup con passport

--- Controllers/sessionsRouteController.js
+++ Controllers/sessionsRouteController.js
@@ -6,24 +6,23 @@ import userModel from "../DAO/models/userModel.js";
 const sessionsRouter = Router();
 const user = new userDB();
 
-export const userSignup = passport.authenticate('signup', {failureRedirect:'/failregister'}) 
- async (req, res)=>{
-    const {first_name, last_name, email, password, age}=req.body;
-    try{
-    const newUser = new userModel({
-        first_name,
-        last_name,
-        email,
-        password: createHash(password),
-        age,
-        rol,
-    })
-    await newUser.save() 
-    res.status(201).json({message:"Usuario creado", data:newUser})
-}catch (error) {
-res.status(500).json({error:error.message})
-}
-};
+export const userSignup = async (req, res, next) => {
+    passport.authenticate('signup', { failureRedirect: '/failregister' }, async (err, user) => {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      if (!user) {
+        return res.status(400).json({ message: 'Failed to create user' });
+      }
+      req.logIn(user, (err) => {
+        if (err) {
+          return res.status(500).json({ error: err.message });
+        }
+        res.status(201).json({ message: 'Usuario Creado', data: user });
+      });
+    })(req, res, next);
+  };
+  
 
 
 export const failRegister = async (req, res)=>{ 
