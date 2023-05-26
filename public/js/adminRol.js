217216582c4088f2fd95bdc5faea4669764c24b7
Lucas Katz <l.katz92@gmail.com>
2023-05-26 14:47:31 -0300

    el admin puede cambiar roles de usuario

--- public/js/adminRol.js
+++ public/js/adminRol.js
@@ -1,33 +1,62 @@
+const rolButtons = document.getElementsByClassName('rolButtonClass');
+const deleteButtons = document.getElementsByClassName('deleteButtonClass');
 
-
-const rolButton = document.getElementById('rolButton');
-
-
-rolButton.addEventListener('click', async (event) => {
-  event.preventDefault();
-
-  // Obtener el ID del usuario del atributo data
-  const userId = event.target.dataset.userId;
-
-  try {
-    // Realizar la solicitud al backend utilizando fetch
-    const response = await fetch('/listOfUsers', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ userId }),
-    });
-
-    // Verificar el estado de la respuesta
-    if (response.ok) {
-      const data = await response.json();
-      // Hacer algo con la respuesta del backend
-      console.log(data);
-    } else {
-      console.error('Error en la solicitud al backend');
+// Agregar controladores de eventos a los botones de cambio de rol
+Array.from(rolButtons).forEach((button) => {
+  button.addEventListener('click', async (event) => {
+    event.preventDefault();
+  
+    // Obtener el ID del usuario del atributo data
+    const userId = event.target.dataset.userId;
+  
+    try {
+      // Realizar la solicitud al backend utilizando fetch
+      const response = await fetch('/listOfUsers', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userId }),
+      });
+  
+      // Verificar el estado de la respuesta
+      if (response.ok) {
+        const data = await response.json();
+        // Hacer algo con la respuesta del backend
+        console.log(data);
+      } else {
+        console.error('Error en la solicitud al backend');
+      }
+    } catch (error) {
+      console.error(error);
     }
-  } catch (error) {
-    console.error(error);
-  }
+  });
+});
+
+// Agregar controladores de eventos a los botones de eliminación
+Array.from(deleteButtons).forEach((button) => {
+  button.addEventListener('click', async (event) => {
+    event.preventDefault();
+  
+    // Obtener el ID del usuario del atributo data
+    const userId = event.target.dataset.userId;
+  
+    try {
+      // Realizar la solicitud al backend utilizando fetch
+      const response = await fetch(`/listOfUsers/${userId}`, {
+        method: 'DELETE',
+      });
+  
+      // Verificar el estado de la respuesta
+      if (response.ok) {
+        const data = await response.json();
+        // Hacer algo con la respuesta del backend
+        console.log(data);
+      } else {
+        console.error('Error en la solicitud al backend');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  });
 });
