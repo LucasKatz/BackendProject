64f7e70db0ef8ec5063147b2b3240d53edee930e
Lucas Katz <l.katz92@gmail.com>
2023-09-03 17:07:40 -0300

    saque toastr

--- public/js/products.js
+++ public/js/products.js
@@ -1,54 +1,47 @@
-// Importa Toastr
-import toastr from 'toastr';
-import '../../views/toastr.css'; 
-
-// Configura opciones globales de Toastr (opcional)
-toastr.options = {
-    closeButton: true, // Muestra un botón de cierre en las notificaciones
-    positionClass: 'toast-top-right', // Posición de las notificaciones en la pantalla
-};
-
-// Agrega un evento click al botón "Agregar al Carrito" usando delegación de eventos
+// Agrego un evento click al botón "Agregar al Carrito" usando delegación de eventos
 document.addEventListener("click", function(event) {
-    console.log("Click event triggered");
     if (event.target.classList.contains("addToCartButton")) {
         const productId = event.target.dataset.productId;
         const quantityInput = event.target.parentNode.querySelector(".productQuantity");
         const quantity = parseInt(quantityInput.value);
 
-        // Obtén el cartId desde sessionStorage
-        const cartId = sessionStorage.getItem("cartId");
+    // Obtengo el cartId desde sessionStorage
+    const cartId = sessionStorage.getItem("cartId");
 
-        // Realiza una solicitud POST al servidor para agregar el producto al carrito
-        fetch(`/api/carts/${cartId}/products/${productId}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ quantity: quantity })
-        })
+
+      // Realiza una solicitud POST al servidor para agregar el producto al carrito
+    fetch(`/api/carts/${cartId}/products/${productId}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ quantity: quantity })
+    })
         .then(response => response.json())
         .then(data => {
-            // Muestra una notificación de éxito con Toastr
-            toastr.success("Producto agregado con éxito");
-
-            // Realiza alguna acción después de agregar el producto al carrito
+          // Realiza alguna acción después de agregar el producto al carrito
             console.log(data);
+            console.log("Producto agregado con éxito");
         })
         .catch(error => {
-            // Muestra una notificación de error con Toastr
-            toastr.error("Error al agregar el producto al carrito");
-
             console.error("Error al agregar el producto al carrito:", error);
         });
     }
 });
 
-// Agrega un evento click al botón "Eliminar" usando delegación de eventos
+
+document.querySelectorAll('.showDetailsButton').forEach(button => {
+    button.addEventListener('click', () => {
+        const container = button.parentNode.parentNode.querySelector('.detailsContainer');
+        container.style.display = container.style.display === 'none' ? 'block' : 'none';
+    });
+});
+
+
+// Agrego un evento click al botón "Eliminar" usando delegación de eventos
 document.addEventListener("click", function(event) {
-    console.log("Click event triggered");
     if (event.target.classList.contains("deleteProductButton")) {
-        // Obtén el cartId desde sessionStorage
+        // Obtengo el cartId desde sessionStorage
         const cartId = sessionStorage.getItem("cartId");
         const productId = event.target.dataset.productId;
 
@@ -61,27 +54,21 @@ document.addEventListener("click", function(event) {
         })
         .then(response => response.json())
         .then(data => {
-            // Muestra una notificación de éxito con Toastr
-            toastr.success("Producto eliminado con éxito");
-
             // Realiza alguna acción después de eliminar el producto del carrito
             console.log(data);
+            console.log("Producto eliminado con éxito");
             location.reload(); // Recarga la página
         })
         .catch(error => {
-            // Muestra una notificación de error con Toastr
-            toastr.error("Error al eliminar el producto del carrito");
-
             console.error("Error al eliminar el producto del carrito:", error);
         });
     }
 });
 
-// Agrega un evento click al botón "Vaciar Carrito" usando delegación de eventos
+// Agrego un evento click al botón "Vaciar Carrito" usando delegación de eventos
 document.addEventListener("click", function(event) {
-    console.log("Click event triggered");
     if (event.target.classList.contains("vaciarCarrito")) {
-        // Obtén el cartId desde el botón
+        // Obtengo el cartId desde el botón
         const cartId = event.target.dataset.cartId;
 
         // Realiza una solicitud DELETE al servidor para vaciar el carrito
@@ -93,17 +80,12 @@ document.addEventListener("click", function(event) {
         })
         .then(response => response.json())
         .then(data => {
-            // Muestra una notificación de éxito con Toastr
-            toastr.success("Carrito vaciado con éxito");
-
             // Realiza alguna acción después de vaciar el carrito
             console.log(data);
+            console.log("Carrito vaciado con éxito");
             location.reload(); // Recarga la página
         })
         .catch(error => {
-            // Muestra una notificación de error con Toastr
-            toastr.error("Error al vaciar el carrito");
-
             console.error("Error al vaciar el carrito:", error);
         });
     }
