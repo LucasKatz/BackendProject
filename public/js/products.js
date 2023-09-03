3e2189801fc785edc50d780ddcf225d04e0812a1
Lucas Katz <l.katz92@gmail.com>
2023-09-03 16:29:45 -0300

    SA try 2

--- public/js/products.js
+++ public/js/products.js
@@ -1,3 +1,6 @@
+import Swal from 'sweetalert2';
+
+
 // Agrego un evento click al botón "Agregar al Carrito" usando delegación de eventos
 document.addEventListener("click", function(event) {
     if (event.target.classList.contains("addToCartButton")) {
@@ -5,30 +8,33 @@ document.addEventListener("click", function(event) {
         const quantityInput = event.target.parentNode.querySelector(".productQuantity");
         const quantity = parseInt(quantityInput.value);
 
-    // Obtengo el cartId desde sessionStorage
-    const cartId = sessionStorage.getItem("cartId");
+        // Obtengo el cartId desde sessionStorage
+        const cartId = sessionStorage.getItem("cartId");
 
-
-      // Realiza una solicitud POST al servidor para agregar el producto al carrito
-    fetch(`/api/carts/${cartId}/products/${productId}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ quantity: quantity })
-    })
+        // Realiza una solicitud POST al servidor para agregar el producto al carrito
+        fetch(`/api/carts/${cartId}/products/${productId}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ quantity: quantity })
+        })
         .then(response => response.json())
         .then(data => {
-          // Realiza alguna acción después de agregar el producto al carrito
-            console.log(data);
-            console.log("Producto agregado con éxito");
+            // Muestra una notificación de éxito con Sweet Alert
+            Swal.fire("Éxito", "Producto agregado con éxito", "success")
+            .then(() => {
+                console.log(data);
+                // Otra acción si es necesaria
+            });
         })
         .catch(error => {
+            // Muestra una notificación de error con Sweet Alert
+            Swal.fire("Error", "Error al agregar el producto al carrito", "error");
             console.error("Error al agregar el producto al carrito:", error);
         });
     }
 });
-
 
 document.querySelectorAll('.showDetailsButton').forEach(button => {
     button.addEventListener('click', () => {
@@ -36,7 +42,6 @@ document.querySelectorAll('.showDetailsButton').forEach(button => {
         container.style.display = container.style.display === 'none' ? 'block' : 'none';
     });
 });
-
 
 // Agrego un evento click al botón "Eliminar" usando delegación de eventos
 document.addEventListener("click", function(event) {
@@ -54,12 +59,16 @@ document.addEventListener("click", function(event) {
         })
         .then(response => response.json())
         .then(data => {
-            // Realiza alguna acción después de eliminar el producto del carrito
-            console.log(data);
-            console.log("Producto eliminado con éxito");
-            location.reload(); // Recarga la página
+            // Muestra una notificación de éxito con Sweet Alert
+            Swal.fire("Éxito", "Producto eliminado con éxito", "success")
+            .then(() => {
+                console.log(data);
+                location.reload(); // Recarga la página
+            });
         })
         .catch(error => {
+            // Muestra una notificación de error con Sweet Alert
+            Swal.fire("Error", "Error al eliminar el producto del carrito", "error");
             console.error("Error al eliminar el producto del carrito:", error);
         });
     }
@@ -80,12 +89,16 @@ document.addEventListener("click", function(event) {
         })
         .then(response => response.json())
         .then(data => {
-            // Realiza alguna acción después de vaciar el carrito
-            console.log(data);
-            console.log("Carrito vaciado con éxito");
-            location.reload(); // Recarga la página
+            // Muestra una notificación de éxito con Sweet Alert
+            swal("Éxito", "Carrito vaciado con éxito", "success")
+            .then(() => {
+                console.log(data);
+                location.reload(); // Recarga la página
+            });
         })
         .catch(error => {
+            // Muestra una notificación de error con Sweet Alert
+            swal("Error", "Error al vaciar el carrito", "error");
             console.error("Error al vaciar el carrito:", error);
         });
     }
