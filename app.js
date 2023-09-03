58e263218511d4e52b30286613dc0ba6020abd57
Lucas Katz <l.katz92@gmail.com>
2023-09-03 17:11:55 -0300

    cambio en config de CORS

--- app.js
+++ app.js
@@ -69,11 +69,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/apidocs', swaggerUiExpress.serve,swaggerUiExpress.setup(specs) )
 // Configuración de CORS
-app.use(cors({
-  origin: "*", 
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true
-}));
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, 	DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
 
 
 app.post("/socketMessage", (req, res) => {
